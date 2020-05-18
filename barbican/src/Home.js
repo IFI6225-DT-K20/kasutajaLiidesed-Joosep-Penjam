160ed7e9231fc1f3c9ae79e7700027471890d9bd
Joosep Penjam <joosep98@tlu.ee>
2020-05-18 14:03:24 +0300

    Changes to Home Page

--- barbican/src/Home.js
+++ barbican/src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { Image, Button, Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import { Dropdown, DropdownButton,Image, Button, Jumbotron as Jumbo, Container } from 'react-bootstrap';
 // eslint-disable-next-line
 import backVideo from './Assets/backVideo.mp4';
 // eslint-disable-next-line
@@ -62,29 +62,39 @@ const Styles = styled.div `
 
     .newsLetter{
         background-color: #ff5900;
-        height: 200px;
+        height: 180px;
         font-weight: bold;
         display: flex;
-        
-        
+    }
+
+    .pictureContainer{
+        margin-left: 30%;
+        margin-right: 0;
     }
 
     #signUp{
-        text-align: center;
-        padding-left: 480px;
+        color: white;
+        font-size: 2rem;
+        padding-left: 40px;
+        
     }
 
     .newsButton{
         color: #ff5900;
         background-color: white;
         border-color: #ff5900;
-        height: 60px;
-        margin-top: 60px;
+        height: 50px;
+        margin-left: 20px;
+        margin-top: 50px;
     }
 
     img{
+        height: 150;
         max-width:100%;
         max-height:100%;
+        margin-right: 0;
+
+       
     }
     
     .footer{
@@ -93,6 +103,30 @@ const Styles = styled.div `
         color:white;
         padding-bottom: 10px;
         margin-bottom: 0;
+
+        h2{
+            padding-top: 30px;
+            font-size: 2.5rem;
+        }
+
+        #first{
+            font-size: 1.5rem;
+        }
+
+        #dropdown-item-button{
+            background-color: #1D1D1D;
+            border-color: white;
+            width: 400px;
+            text-align: left;
+        }
+
+        h3{
+
+        }
+
+        #second{
+            
+        }
     }
 `;
 
@@ -115,20 +149,28 @@ export const Home = () => (
         </Jumbo>
 
         <div className="newsLetter">
+            <div className="pictureContainer">
+                <Image src={newsletters}></Image>
+            </div>
+
             <p id="signUp">  Sign up to our newsletter</p>
-            <Image src={newsletters}></Image>
             <Button className="newsButton" variant="primary" size="lg">
-                    -> Sign up
+                        -> Sign up
             </Button>{' '}
         </div>
 
         <div className="footer">
             <h2>Contact us</h2>
-            <p>About</p>
+            <p id="first">About</p>
+            <DropdownButton id="dropdown-item-button" title="Your booking">
+                <Dropdown.Item as="button">Feedback</Dropdown.Item>
+                <Dropdown.Item as="button">Membership</Dropdown.Item>
+                <Dropdown.Item as="button">Hiring value</Dropdown.Item>
+            </DropdownButton>
             <h3>Contact the Box office</h3>
-            <p>For queries relating to you booking, please see our FAQs or contact us on:</p>
+            <p id="second">For queries relating to you booking, please see our FAQs or contact us on:</p>
             <h4>tickets@barbican.org.uk</h4>
-            <p>We aim to respong within 24 hrs</p>
+            <p id="third">We aim to respong within 24 hrs</p>
         </div>
     </Styles>
 )
